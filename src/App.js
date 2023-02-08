f178307dd7c62fbda7c3da890f7945d299eefdca
azat <arykov.azat.kg@gmail.com>
2023-02-08 18:49:06 +0600

    add data to localStorage and add db.json

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/header/Header';
 import Main from './components/main/Main';
 import OurMenuPage from './components/ourMenuPage/OurMenuPage';
 import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { data } from './components/data/data';
 
 function App() {
@@ -36,9 +36,11 @@ function App() {
         el.id === item.id ? { ...existData, total: existData.total + 1 } : el,
       );
       setEmptyBasketData(newData);
+      localStorage.setItem('basket', JSON.stringify(newData));
     } else {
       const newData = [...emptyBasketData, { ...item, total: 1 }];
       setEmptyBasketData(newData);
+      localStorage.setItem('basket', JSON.stringify(newData));
     }
   };
 
@@ -47,21 +49,32 @@ function App() {
     if (existData.total === 1) {
       const newData = emptyBasketData.filter((el) => el.id !== item.id);
       setEmptyBasketData(newData);
+      localStorage.setItem('basket', JSON.stringify(newData));
     } else {
       const newData = emptyBasketData.map((el) =>
         el.id === item.id ? { ...existData, total: existData.total - 1 } : el,
       );
       setEmptyBasketData(newData);
+      localStorage.setItem('basket', JSON.stringify(newData));
     }
   };
+
   const onRemoveItem = (id) => {
-    setEmptyBasketData((el) => emptyBasketData.filter((el) => el.id !== id));
+    const newData = emptyBasketData.filter((el) => el.id !== id);
+    setEmptyBasketData(newData);
+    localStorage.setItem('basket', JSON.stringify(newData));
   };
 
   const onClearCardData = () => {
     setEmptyBasketData([]);
   };
 
+  useEffect(() => {
+    setEmptyBasketData(
+      localStorage.getItem('basket') ? JSON.parse(localStorage.getItem('basket')) : [],
+    );
+  }, []);
+  
   return (
     <div className="wrapper">
       <Header totalPrice={totalPrice} totalCount={totalCount} />
